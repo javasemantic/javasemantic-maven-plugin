ab7fd49a962cd5f8f7a55acd4bfff458b1391e89
Coenraad Human <coen.human@gmail.com>
2021-09-21 18:25:31 +0200

    feature: add in our version mojo

---
+++ src/main/java/io/github/javasemantic/DetermineVersionMojo.java
@@ -0,0 +1,39 @@
+package io.github.javasemantic;
+
+import io.github.javasemantic.utility.ValidConventionalCommitUtil;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "determine-version")
+public class DetermineVersionMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${git.commit.message}", required = true )
+    private String commitMessage;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        executeCommitMessageValidation();
+        executeVersionCalculation();
+    }
+
+    private void executeVersionCalculation() {
+        var service = JavaSemanticServiceFactory.get();
+        var version = service.execute();
+        System.out.println(version);
+    }
+
+    private void executeCommitMessageValidation() throws MojoExecutionException {
+        var result = ValidConventionalCommitUtil.isValid(commitMessage);
+
+        if (result) {
+            getLog().info(ValidConventionalCommitUtil.VALID_LOG_MESSAGE);
+        } else {
+            throw new MojoExecutionException(
+                    commitMessage + ValidConventionalCommitUtil.INVALID_LOG_MESSAGE
+            );
+        }
+    }
+
+}
