a2ca5cce712c7f7894fbffad6627b811e84353fe
Matteo Lovisa <matlo.lo27@gmail.com>
2021-09-25 15:33:50 +0200

    feat: include last commit

--- src/main/java/io/github/javasemantic/DetermineVersionMojo.java
+++ src/main/java/io/github/javasemantic/DetermineVersionMojo.java
@@ -1,16 +1,23 @@
 package io.github.javasemantic;
 
-import io.github.javasemantic.utility.ValidConventionalCommitUtil;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.util.List;
+
+import io.github.javasemantic.domain.model.DirtyCommit;
+import io.github.javasemantic.utility.ValidConventionalCommitUtil;
+
 @Mojo(name = "determine-version")
 public class DetermineVersionMojo extends AbstractMojo {
 
-    @Parameter(defaultValue = "${git.commit.message}", required = true )
+    @Parameter(defaultValue = "${git.commit.message}", required = true)
     private String commitMessage;
+
+    @Parameter(defaultValue = "${git.commit.body}", required = true)
+    private String commitBody;
 
     @Override
     public void execute() throws MojoExecutionException {
@@ -19,9 +26,18 @@ public class DetermineVersionMojo extends AbstractMojo {
     }
 
     private void executeVersionCalculation() {
-        var service = JavaSemanticServiceFactory.get(getLog());
+        getLog().info("Last commit message: " + commitMessage);
+        getLog().info("Last commit body: " + commitBody);
+        var service = JavaSemanticServiceFactory.get(getLog(), buildLastCommit());
         var version = service.execute();
         System.out.println(version);
+    }
+
+    private DirtyCommit buildLastCommit() {
+        return DirtyCommit.builder()
+            .message(commitMessage)
+            .footers(List.of(commitBody))
+            .build();
     }
 
     private void executeCommitMessageValidation() throws MojoExecutionException {
@@ -31,7 +47,7 @@ public class DetermineVersionMojo extends AbstractMojo {
             getLog().info(ValidConventionalCommitUtil.VALID_LOG_MESSAGE);
         } else {
             throw new MojoExecutionException(
-                    commitMessage + ValidConventionalCommitUtil.INVALID_LOG_MESSAGE
+                commitMessage + ValidConventionalCommitUtil.INVALID_LOG_MESSAGE
             );
         }
     }
