680dc2727ee6d0939100750c5ee167d067c99afd
FuzzyMongoose <74914310+FuzzyMongoose@users.noreply.github.com>
2021-09-26 15:59:53 +0200

    feat: add version update (#8)

--- src/main/java/io/github/javasemantic/DetermineVersionMojo.java
+++ src/main/java/io/github/javasemantic/DetermineVersionMojo.java
@@ -1,8 +1,8 @@
 package io.github.javasemantic;
 
-import io.github.javasemantic.domain.model.DirtyCommit;
-import io.github.javasemantic.utility.ValidConventionalCommitUtil;
-import io.github.javasemantic.version.updater.VersionUpdaterFactory;
+
+import org.apache.commons.lang3.StringUtils;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -12,13 +12,17 @@ import org.apache.maven.project.MavenProject;
 import java.util.List;
 import java.util.function.UnaryOperator;
 
+import io.github.javasemantic.domain.model.DirtyCommit;
+import io.github.javasemantic.logging.Log;
+import io.github.javasemantic.utility.GitRepositoryFactory;
+import io.github.javasemantic.utility.ValidConventionalCommitUtil;
+import io.github.javasemantic.version.updater.VersionUpdaterFactory;
+
 import static io.github.javasemantic.PathFinder.MAVEN_HOME_PROP;
 import static io.github.javasemantic.PathFinder.findMavenToolPath;
 
 @Mojo(name = "determine-version")
 public class DetermineVersionMojo extends AbstractMojo {
-
-    private boolean isSnapshot = true;
 
     private final String POM_FILE = "pom.xml";
     private final UnaryOperator<String> systemProperties = System::getProperty;
@@ -32,40 +36,61 @@ public class DetermineVersionMojo extends AbstractMojo {
     @Parameter(readonly = true, defaultValue = "${project}")
     private MavenProject currentProject;
 
+    @Parameter(property = "version.override.activate", defaultValue = "false", name = "activate-override-version")
+    private boolean activateOverrideVersion;
+
+    @Parameter(property = "version.override", defaultValue = "0.0.1-SNAPSHOT", name = "override-static-version")
+    private String overrideStaticVersion;
+
+    @Parameter(property = "override.version.branch.blacklist.items", name = "override-version-branch-blacklist-items")
+    private List<String> overrideVersionBranchBlacklistItems;
+
     @Override
     public void execute() throws MojoExecutionException {
         try {
             executeCommitMessageValidation();
-
-            // Todo: add in snapshot version option and value into plugin setting in pom.xml
-            if (isSnapshot) {
-                executePomVersionUpdate("0.0.5-SNAPSHOT");
-            } else {
-                executePomVersionUpdate(
-                        executeVersionCalculation()
-                );
-            }
-
+            executeUpdateVersion();
             // Hook: post-commit will add amended build file to commit.
         } catch (Exception e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
     }
 
+    private void executeUpdateVersion() {
+
+        if (isOverrideVersion()) {
+            executePomVersionUpdate(overrideStaticVersion);
+        } else {
+            executePomVersionUpdate(
+                executeVersionCalculation()
+            );
+        }
+    }
+
+    private boolean isOverrideVersion() {
+
+        var currentWorkingBranch = GitRepositoryFactory.currentWorkingBranch();
+        Log.info(this.getClass(), "Current working branch: " + currentWorkingBranch);
+        Log.info(this.getClass(), "Black list branches: " + overrideVersionBranchBlacklistItems);
+        var overrideBranch = overrideVersionBranchBlacklistItems.stream().noneMatch(currentWorkingBranch::contains);
+
+        return activateOverrideVersion && overrideBranch;
+    }
+
     private String executeVersionCalculation() {
-        getLog().info("Last commit message: " + commitMessage);
-        getLog().info("Last commit body: " + commitBody);
+        Log.info(this.getClass(), "Last commit message: " + commitMessage);
+
+        if (StringUtils.isNotBlank(commitBody))
+            Log.info(this.getClass(), "Last commit body: " + commitBody);
+
         var service = JavaSemanticServiceFactory.get(getLog(), buildLastCommit());
         var version = service.execute();
-        getLog().info("Version calculated for project: " + version.toString());
         return version.toString();
-
     }
 
     private void executePomVersionUpdate(String version) {
         var versionUpdater = VersionUpdaterFactory.getMavenVersionUpdater();
         var mavenBin = findMavenToolPath(systemProperties.apply(MAVEN_HOME_PROP), getClass());
-        getLog().info("Update Project Version: " + commitBody);
         versionUpdater.updateVersion(mavenBin, currentProject.getBasedir().toPath(), version);
     }
 
@@ -80,7 +105,7 @@ public class DetermineVersionMojo extends AbstractMojo {
         var result = ValidConventionalCommitUtil.isValid(commitMessage);
 
         if (result) {
-            getLog().info(ValidConventionalCommitUtil.VALID_LOG_MESSAGE);
+            Log.info(this.getClass(), ValidConventionalCommitUtil.VALID_LOG_MESSAGE);
         } else {
             throw new MojoExecutionException(
                 commitMessage + ValidConventionalCommitUtil.INVALID_LOG_MESSAGE
